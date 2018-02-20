303d6b6623a8d3b1e6d79f3e39e085c0fa3d1d8e
kadekchresna <kadekchresna@gmail.com>
2018-02-21 03:48:47 +0700

    commit awal

---
+++ functions/index.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.sendNotification = functions.database.ref('/notifications/{user_id}/{notification_id}').onWrite(event =>{
+	const user_id = event.params.user_id;
+	const notification = event.params.notification;
+
+	console.log('We have a notification to send to: ',user_id);
+	
+	if (!event.data.val()) {
+
+		return console.log('A notification has been deleted from database: ',notification_id);
+
+	}
+
+	const deviceToken = admin.database().ref(`/Users/${user_id}/device_token`).once('value');
+
+	return deviceToken.then(result =>{
+		const token_id = result.val();
+
+		const payload = {
+
+			notification:{
+				title: "Firend Request",
+				body: "You get a friend request",
+				icon: "default"
+			}
+
+		};
+
+		return admin.messaging().sendToDevice(token_id, payload).then(response =>{
+			console.log('This was the notification Feature');
+		});
+
+	});
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });

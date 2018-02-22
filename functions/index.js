7faf93c18162cec00f11688b885afbbf1fffd790
kadekchresna <kadekchresna@gmail.com>
2018-02-22 17:27:53 +0700

    completed adding the information to push notification

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendNotification = functions.database.ref('/notifications/{user_id}/{notification_id}').onWrite(event =>{
 	const user_id = event.params.user_id;
-	const notification = event.params.notification;
+	const notification_id = event.params.notification_id;
 
 	console.log('We have a notification to send to: ',user_id);
 	
@@ -17,26 +17,47 @@ exports.sendNotification = functions.database.ref('/notifications/{user_id}/{not
 
 	}
 
-	const deviceToken = admin.database().ref(`/Users/${user_id}/device_token`).once('value');
+	const fromUser = admin.database().ref(`/notifications/${user_id}/${notification_id}`).once('value');
+	return fromUser.then(fromUserResult =>{
+		const from_user_id = fromUserResult.val().from;
+		console.log('You have notification from : ',from_user_id);
 
-	return deviceToken.then(result =>{
-		const token_id = result.val();
+		const userQuery = admin.database().ref(`Users/${from_user_id}`).once('value');
+		return userQuery.then(userResult =>	{
 
-		const payload = {
+			const userName = userResult.val().name;
+			console.log('You have notification from : ',userName);
 
-			notification:{
-				title: "Firend Request",
-				body: "You get a friend request",
-				icon: "default"
-			}
+			const deviceToken = admin.database().ref(`/Users/${user_id}/device_token`).once('value');
 
-		};
+			return deviceToken.then(result =>{
+				const token_id = result.val();
+				console.log('Token id is: ',token_id);
 
-		return admin.messaging().sendToDevice(token_id, payload).then(response =>{
-			console.log('This was the notification Feature');
+				const payload = {
+
+					notification:{
+						title: "Firend Request",
+						body: `${userName} has send you friend request`,
+						icon: "default",
+						click_action: "com.kadek.chatsys_TARGET_NOTIFICATION"
+					},
+					data:{
+						from_user_id: from_user_id
+					}
+
+				};
+
+				return admin.messaging().sendToDevice(token_id, payload).then(response =>{
+					console.log('This was the notification Feature');
+				});
+
+			});	
+
 		});
 
 	});
+	
 });
 
 // // Create and Deploy Your First Cloud Functions
